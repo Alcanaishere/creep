f0a389d4f874c70159bccfb37c77a2441940d347
Alcanaishere <a1canaph@gmail.com>
2026-04-09 19:29:44 +0900

    Add current_level initialization and retrieval
    Initialize current_level and retrieve from localStorage if available.

--- codes/home/script.js
+++ codes/home/script.js
@@ -1,1 +1,9 @@
+let current_level = 0;
+if (localStrage.getItem('level') != b'')
+{let current_level = localStrage.getItem('level');}
+//levelをポイントごとに加算↓
+//localStrage.setItem('level','変数')
+
+
+
 console.log("hello yuunagi");
